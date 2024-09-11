e40e571b946d096e19f16370f43c3629e5e8fbeb
Filipelaw45 <filipe.law@gmail.com>
2024-09-11 11:57:36 -0300

    feat: criado rotas para páginas

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { Trip } from './pages/Trip';
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}>
+      <Route>
         <Route path="/" element={<Home />} />
+        <Route path="/viagem" element={<Trip />} />
       </Route>
 
       <Route path="*" element={<div>Página não encontrada</div>} />
