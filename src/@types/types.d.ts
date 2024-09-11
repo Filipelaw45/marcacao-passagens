850b8ee798f6f5d46d871ba1af33ec8b4e50e537
Filipelaw45 <filipe.law@gmail.com>
2024-09-11 11:56:17 -0300

    feat: criado tipagem de viagem e cidade

---
+++ src/@types/types.d.ts
@@ -0,0 +1,36 @@
+interface City {
+  nome: string;
+  codigo_ibge: string;
+}
+
+interface Passenger {
+  seat: number;
+  fullName: string;
+  rg: string;
+  sex: 'F' | 'M',
+  origin: {
+    city: string;
+    uf: string;
+  };
+  destination: {
+    city: string;
+    uf: string;
+  };
+  notes: string;
+  value: number;
+  escort: number;
+}
+
+interface Trip {
+  departureDay: string;
+  returnDay: string;
+  busModel: '40' | '42' | '64';
+  busNumber: string;
+  driver: string;
+  team: string;
+  originUf: string;
+  originCity: string;
+  destinationUf: string;
+  destinationCity: string;
+  passengers: Passenger[];
+}
