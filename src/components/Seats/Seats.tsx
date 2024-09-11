1adaa7189033495ced7381e5c94e9b85b705a3fc
Makawlly <136134413+Mescaule@users.noreply.github.com>
2024-09-11 11:40:49 -0300

    feat: criado componente `Seats` para criar cada assento individualmente e configurado suas props iniciais

---
+++ src/components/Seats/Seats.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+interface SeatProps {
+  id: number;
+  width?: string;
+  height?: string;
+  sex?: string;
+  isPreferential?: boolean;
+}
+
+const Seats: React.FC<SeatProps> = ({
+  id,
+  width = "w-11",
+  height = "h-9",
+  sex,
+  isPreferential
+}) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(true);
+    setTimeout(() => {
+      setIsClicked(false);
+    }, 200);
+  };
+
+  const getBackgroundColor = () => {
+    if (sex === "M") return "bg-blue-950"
+    if (sex === "F") return "bg-pink-600"
+    return "bg-green-700"
+  };
+
+  return (
+    <div className="flex items-center justify-center">
+      <button
+        id={`${id}`}
+        onClick={handleClick}
+        className={`${width} ${height} m-[.2rem] flex items-center justify-center text-white rounded-lg relative transform transition-transform duration-200 
+        ${isClicked ? "scale-95 opacity-80" : "scale-100"}
+        ${isPreferential ? "bg-amber-500" : "bg-green-700"}
+        ${getBackgroundColor()}`}
+      >
+        <div
+          className="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-blue-800 rounded-t-lg border-white border"
+          style={{
+            width: "80%",
+            height: "20%",
+          }}
+        ></div>
+        {id}
+      </button>
+    </div>
+  );
+};
+
+export default Seats;
